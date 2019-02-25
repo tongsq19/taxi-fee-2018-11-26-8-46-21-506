2a1d8d78ead44eedbb395e1d330c11a14d26331a
ubinexy <ubinexy@gmail.com>
2019-02-25 19:08:32 +0800

    Add 1st Test

--- main/main.js
+++ main/main.js
@@ -3,18 +3,22 @@
 //     return 'Hello World!';
 // };
 
+// module.exports = function main(distance_in_km, time_in_minute) {
+//     var base_fee = 6;
+//
+//     var fee_per_minute = 0.25;
+//     var fee_per_distance1 = 0.8;
+//     var fee_per_distance2 = 1.2;
+//
+//     if(distance_in_km <= 2) {
+//         return Math.round(base_fee + fee_per_minute * time_in_minute);
+//     } else if(distance_in_km <= 8){
+//         return Math.round(base_fee + fee_per_minute * time_in_minute + fee_per_distance1 * (distance_in_km - 2) );
+//     } else if(distance_in_km > 8){
+//         return Math.round(base_fee + fee_per_minute * time_in_minute + fee_per_distance1 * (8 - 2) + fee_per_distance2 * (distance_in_km - 8));
+//     }
+// }
+
 module.exports = function main(distance_in_km, time_in_minute) {
-    var base_fee = 6;
-
-    var fee_per_minute = 0.25;
-    var fee_per_distance1 = 0.8;
-    var fee_per_distance2 = 1.2;
-
-    if(distance_in_km <= 2) {
-        return Math.round(base_fee + fee_per_minute * time_in_minute);
-    } else if(distance_in_km <= 8){
-        return Math.round(base_fee + fee_per_minute * time_in_minute + fee_per_distance1 * (distance_in_km - 2) );
-    } else if(distance_in_km > 8){
-        return Math.round(base_fee + fee_per_minute * time_in_minute + fee_per_distance1 * (8 - 2) + fee_per_distance2 * (distance_in_km - 8));
-    }
-}
+    return 0;
+};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -8,26 +8,36 @@ chai.use(sinonChai);
 
 const main = require('../main/main.js');
 
-describe('taxi fee', function () {
-    // write your tests here...
-    it("return 6, if distance is less than 2 kilometers and wait time is less than 2 minutes.", function() {
-        var distance = 1;
-        var time = 1;
-        var result = main(distance, time);
-        expect(result).to.equal(6);
-    });
-
-    it("return 7, if distance is 3 kilometers, and wait time is is less than 2.8 minutes.", function(){
-        var distance = 3;
-        var time = 2.79;
-        var result = main(distance, time);
-        expect(result).to.equal(7);
-    });
-
-    it("return 13, if distance is 10 kilometers, and wait time is is less than 1.2 minutes.", function(){
-        var distance = 10;
-        var time = 1;
-        var result = main(distance, time);
-        expect(result).to.equal(13);
-    });
+describe('taxi fee', function() {
+   it("2公里内，起步费6元", function() {
+       let distance = 0;
+       let time = 0;
+       let expected = 6;
+       let result = main(distance, time);
+       expect(result).to.equal(expected);
+   })
 });
+
+// describe('taxi fee', function () {
+//     // write your tests here...
+//     it("return 6, if distance is less than 2 kilometers and wait time is less than 2 minutes.", function() {
+//         var distance = 1;
+//         var time = 1;
+//         var result = main(distance, time);
+//         expect(result).to.equal(6);
+//     });
+//
+//     it("return 7, if distance is 3 kilometers, and wait time is is less than 2.8 minutes.", function(){
+//         var distance = 3;
+//         var time = 2.79;
+//         var result = main(distance, time);
+//         expect(result).to.equal(7);
+//     });
+//
+//     it("return 13, if distance is 10 kilometers, and wait time is is less than 1.2 minutes.", function(){
+//         var distance = 10;
+//         var time = 1;
+//         var result = main(distance, time);
+//         expect(result).to.equal(13);
+//     });
+// });
